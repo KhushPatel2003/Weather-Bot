7b0aff868d4e185899d5b38e40e6200286491ea6
Khush Patel <k425pate@uwaterloo.ca>
2021-12-20 13:01:02 -0500

    first commit

--- main.py
+++ main.py
@@ -1,31 +1,50 @@
 import discord
-
-#ACTUAL CODE FOR WEATHER
+from discord.ext import commands
 import pyowm 
 owm = pyowm.OWM('79a74b9e386227858d196ccc2d008fe4')
-city = 'Niagara Falls, CA'
-loc = owm.weather_manager().weather_at_place(city)
-weather = loc.weather
-temp = weather.temperature(unit='celsius')
-status = weather.detailed_status
-cleaned_temp_data = (int(temp['temp']))
-reply1 = "The temperature in " + str(city) + " is " + str(cleaned_temp_data) + u"\u2103"
-reply2 = "The day today will have " + str(status) + "."
+
+client = commands.Bot(command_prefix='!', help_command = None)
+
+@client.command()
+async def weather(ctx, x):
+    city = x
+    loc = owm.weather_manager().weather_at_place(city)
+    weather = loc.weather
+    temp = weather.temperature(unit='celsius')
+    status = weather.detailed_status
+    cleaned_temp_data = (int(temp['temp']))
+    reply1 = "The temperature in " + str(city) + " is " + str(cleaned_temp_data) + u"\u2103"
+    reply2 = "The day today will have " + str(status) + "."
+    reply3 = str(reply1) + ' ' + str(reply2)
+    result = reply3
+    await ctx.send(result)
+
+# #ACTUAL CODE FOR WEATHER
+# import pyowm 
+# owm = pyowm.OWM('79a74b9e386227858d196ccc2d008fe4')
+# city = 'Niagara Falls, CA'
+# loc = owm.weather_manager().weather_at_place(city)
+# weather = loc.weather
+# temp = weather.temperature(unit='celsius')
+# status = weather.detailed_status
+# cleaned_temp_data = (int(temp['temp']))
+# reply1 = "The temperature in " + str(city) + " is " + str(cleaned_temp_data) + u"\u2103"
+# reply2 = "The day today will have " + str(status) + "."
 
 
-client = discord.Client()
+# client = discord.Client()
 
-#@client.event
-#async def on_ready():
-#    weather_bot_channel = client.get_channel(838887198166220811)
-#    await weather_bot_channel.send("test1")
+# #@client.event
+# #async def on_ready():
+# #    weather_bot_channel = client.get_channel(838887198166220811)
+# #    await weather_bot_channel.send("test1")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith("$"):
-        await message.channel.send(reply1)
-        await message.channel.send(reply2)
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+#     if message.content.startswith("$"):
+#         await message.channel.send(reply1)
+#         await message.channel.send(reply2)
 
-client.run('ODM4ODg1NzM1MTg0MDcyNzI0.YJBnOQ.s-Nk2Uzr7bBpbKofot7OW6cufk0')
+client.run('ODM4ODg1NzM1MTg0MDcyNzI0.YJBnOQ.jojQbZMErWquxox3SvCB1Dz9iDI')
